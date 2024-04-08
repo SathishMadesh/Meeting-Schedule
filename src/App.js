5c7a5b76f37abef28aaef14229fcda555caa563e
SathishMadesh <s747272946@gmail.com>
2024-04-08 15:05:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Tailwind CSS wit react</h1>
+    </div>
+  );
+}
+
+export default App;
