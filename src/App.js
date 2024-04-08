96efb875605ee222831af28ce1bde6d91a09b00a
SathishMadesh <s747272946@gmail.com>
2024-04-08 15:35:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
 import './App.css';
+import {generateDate} from "./util/calendar";
 
 function App() {
+  console.log(generateDate());
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Tailwind CSS wit react</h1>
