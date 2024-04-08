9ef22f3cec87a9f250ef6cd3ee1fa9207fd57a06
SathishMadesh <s747272946@gmail.com>
2024-04-08 16:53:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import {generateDate} from "./util/calendar";
+import cn from './util/cn';
 
 function App() {
   console.log(generateDate());
 
+  const days = ["S","M","T","W","T","F","S"];
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Tailwind CSS wit react</h1>
+    <div className="w-96 h-96">
+      <div className="w-full grid grid-cols-7">
+        {days.map((day,index)=>{
+          return (
+            <h1 key={index} className="h-14 grid place-content-center text-sm">
+              {day}
+            </h1>
+          );
+        })}
+      </div>
+    <div className="w-full grid grid-cols-7">
+      {generateDate().map(({date, currentMonth, today}, index) => {
+        return(
+          <div className="h-14 border-t grid place-content-center text-sm">
+            <h1 key={index} 
+            className={cn(currentMonth?"":"text-gray-400")}>
+              {date.date()}
+            </h1>
+          </div>
+        )
+      })}
+    </div>
     </div>
   );
 }
--- src/util/calendar.js
+++ src/util/calendar.js
@@ -1,12 +1,43 @@
 import dayjs from "dayjs";
 
 export const generateDate = (
-    month = dayjs().month,
+    month = dayjs().month(),
     year = dayjs().year()
 ) => {
     const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
 
     const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
 
-    return [firstDateOfMonth, lastDateOfMonth];
+    const arrayOfDate = [];
+
+    //create prefix date
+    for (let i = 0; i< firstDateOfMonth.day(); i++){
+        arrayOfDate.push({
+            currentMonth: false, 
+            date: firstDateOfMonth.day(i)
+        });
+    }
+
+
+    //generate current Date
+    for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+        arrayOfDate.push({
+            currentMonth: true,
+            date: firstDateOfMonth.date(i),
+            today: firstDateOfMonth.date(i).toDate().toDateString() === 
+            dayjs().toDate().toDateString()
+        });
+    }
+
+    //create sufix date
+    const remaining = 42 - arrayOfDate.length
+    
+    for (let i = lastDateOfMonth.date()+1; i <= lastDateOfMonth.date()+remaining; i++){
+        arrayOfDate.push({
+            currentMonth: false,
+            date: lastDateOfMonth.date(i)
+        });
+    }
+
+    return arrayOfDate;
 };
